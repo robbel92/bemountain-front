bcae4753227f80e4802306ccf76fb1c24ef35816
Robertt Belenguer Cotaina <rob.belenguer@gmail.com>
2023-05-29 18:55:16 +0200

    Create ContainerStyled

---
+++ src/components/shared/ContainerStyled.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const ContainerStyled = styled.div`
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0px 20px 20px 20px;
+`;
+
+export default ContainerStyled;
