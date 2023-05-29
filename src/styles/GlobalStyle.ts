b1930f63902d597704a6e2cf41ed8377f9215fcc
Robertt Belenguer Cotaina <rob.belenguer@gmail.com>
2023-05-29 18:15:34 +0200

    Create GlobalStyle

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+*,::before,::after{
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    font-family: "Montserrat";
+    background-color:${(props) => props.theme.colors.background}
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p {
+    margin: 0;
+    font: inherit;
+  }
+
+  a{
+    text-decoration: none;
+    color: inherit;
+  }
+
+  ul{
+    list-style: none;
+    padding-left: 0;
+    margin-bottom: 0;
+  }
+
+  button {
+    border: none;
+    cursor: pointer;
+   
+  }
+
+
+
+`;
+
+export default GlobalStyle;
