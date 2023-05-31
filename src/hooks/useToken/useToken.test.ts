94ce3fd703d56aeff3f6964d933e63bc9abc4f17
Robertt Belenguer Cotaina <rob.belenguer@gmail.com>
2023-05-31 12:12:19 +0200

    Test getTokenData

---
+++ src/hooks/useToken/useToken.test.ts
@@ -0,0 +1,21 @@
+import { renderHook } from "@testing-library/react";
+import { userDataTokenMock } from "../../mocks/userMocks/userMocks";
+import useToken from "./useToken";
+
+describe("Given a getTokenData function", () => {
+  describe("When it receives a token", () => {
+    test("Then it should return user data from the token received", () => {
+      const token = userDataTokenMock.token;
+
+      const {
+        result: {
+          current: { getTokenData },
+        },
+      } = renderHook(() => useToken());
+
+      const userLoggedData = getTokenData(token);
+
+      expect(userLoggedData).toStrictEqual(userDataTokenMock);
+    });
+  });
+});
