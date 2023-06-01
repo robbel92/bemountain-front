db8e5d2d575a8f8c53713b62dfc77da1b67ee339
Robertt Belenguer Cotaina <rob.belenguer@gmail.com>
2023-06-01 18:01:18 +0200

    Test other branch conditional rendering

--- src/components/Layout/Layout.test.tsx
+++ src/components/Layout/Layout.test.tsx
@@ -2,6 +2,15 @@ import { screen } from "@testing-library/react";
 import { renderWithProviders, wrapWithRouter } from "../../utils/testUtils";
 import Layout from "./Layout";
 import { vi } from "vitest";
+import {
+  Navigate,
+  RouteObject,
+  RouterProvider,
+  createMemoryRouter,
+} from "react-router-dom";
+import { paths } from "../../routers/paths/paths";
+import App from "../App/App";
+import LoginPage from "../pages/LoginPage/LoginPage";
 
 afterEach(() => {
   vi.clearAllMocks();
@@ -17,6 +26,39 @@ describe("Given a Layout component", () => {
       const imageExpected = screen.getByAltText(altTextImage);
 
       expect(imageExpected).toBeInTheDocument();
+    });
+  });
+  describe("When it is rendered and path location is '/login'", () => {
+    test("Then it should show a heading with test 'Log in to enjoy the mountains'", () => {
+      const routes: RouteObject[] = [
+        {
+          path: "/",
+          element: <App />,
+          children: [
+            {
+              index: true,
+              element: <Navigate to={paths.login} replace />,
+            },
+            {
+              path: paths.login,
+              element: <LoginPage />,
+            },
+          ],
+        },
+      ];
+
+      const layoutRouter = createMemoryRouter(routes);
+      const headingText = "Log in to enjoy the mountains";
+
+      renderWithProviders(
+        <RouterProvider router={layoutRouter}></RouterProvider>
+      );
+      const expectedHeading = screen.getByRole("heading", {
+        name: headingText,
+        level: 1,
+      });
+
+      expect(expectedHeading).toBeInTheDocument();
     });
   });
 });
