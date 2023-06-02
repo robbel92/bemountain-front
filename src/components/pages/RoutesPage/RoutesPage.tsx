53cd61b0b3b545d65bed3eab14ba87826b39e3a7
Robertt Belenguer Cotaina <rob.belenguer@gmail.com>
2023-06-02 12:37:26 +0200

    Dispatch routes on RoutesPage

--- src/components/pages/RoutesPage/RoutesPage.tsx
+++ src/components/pages/RoutesPage/RoutesPage.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
+import { routesMock } from "../../../mocks/routeMocks/routeMocks";
+import { useAppDispatch } from "../../../store";
+import { loadRoutesActionCreator } from "../../../store/routes/routesSlice";
 import RoutesPageStyled from "./RoutesPageStyled";
 
 const RoutesPage = (): React.ReactElement => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    (async () => {
+      dispatch(loadRoutesActionCreator(routesMock));
+    })();
+  }, [dispatch]);
+
   return (
     <RoutesPageStyled>
       <h2>World Routes</h2>
