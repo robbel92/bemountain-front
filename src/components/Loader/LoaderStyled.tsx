951b771507b12a41e23b6b1ed182638e31a49ff3
Robertt Belenguer Cotaina <rob.belenguer@gmail.com>
2023-06-03 22:42:34 +0200

    Create and style Loader component

---
+++ src/components/Loader/LoaderStyled.tsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+const LoaderStyled = styled.div`
+  background-color: ${(props) => props.theme.colors.background + 70};
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  .loader {
+    width: 58px;
+    height: 58px;
+    border: 4px solid #fff;
+    border-radius: 50%;
+    display: inline-block;
+    position: relative;
+    box-sizing: border-box;
+    animation: rotation 1s linear infinite;
+  }
+  .loader::after {
+    content: "";
+    box-sizing: border-box;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 76px;
+    height: 76px;
+    border-radius: 50%;
+    border: 6px solid;
+    border-color: ${(props) => props.theme.colors.button} transparent;
+  }
+
+  @keyframes rotation {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+export default LoaderStyled;
