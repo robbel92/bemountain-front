d5dc7f664e83297f912205131a15b2f38dd93884
Robertt Belenguer Cotaina <rob.belenguer@gmail.com>
2023-06-03 12:51:18 +0200

    Test RouteCard component

---
+++ src/components/RouteCard/RouteCard.test.tsx
@@ -0,0 +1,21 @@
+import { renderWithProviders, wrapWithRouter } from "../../utils/testUtils";
+
+import { screen } from "@testing-library/react";
+import RouteCard from "./RouteCard";
+import { routesMock } from "../../mocks/routeMocks/routeMocks";
+
+describe("Given a RouteCard component", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show a heading with the name of the route", () => {
+      const nameRoute = routesMock[0].name;
+      renderWithProviders(wrapWithRouter(<RouteCard route={routesMock[0]} />));
+
+      const expectedHeading = screen.getByRole("heading", {
+        name: nameRoute,
+        level: 2,
+      });
+
+      expect(expectedHeading).toBeInTheDocument();
+    });
+  });
+});
