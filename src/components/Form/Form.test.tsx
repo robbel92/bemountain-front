b3d110c8d011902fa4ba3c2a9938da1bb59c8063
Robertt Belenguer Cotaina <rob.belenguer@gmail.com>
2023-06-07 18:46:08 +0200

    Test case onchange function Form component

--- src/components/Form/Form.test.tsx
+++ src/components/Form/Form.test.tsx
@@ -1,19 +1,24 @@
 import { renderWithProviders, wrapWithRouter } from "../../utils/testUtils";
 import { screen } from "@testing-library/react";
 import Form from "./Form";
+import userEvent from "@testing-library/user-event";
 
 describe("Given a Form component", () => {
-  describe("When it is rendered", () => {
-    test("Then it should show a name text field", () => {
+  describe("When it is rendered and the user types 'Admin' on name text field", () => {
+    test("Then it should show a name text field with 'Admin' value", async () => {
       const labelName = "Name";
+      const name = "Admin";
 
       renderWithProviders(wrapWithRouter(<Form />));
 
       screen.debug();
-
       const nameTextField = screen.getByLabelText(labelName);
 
+      await userEvent.type(nameTextField, name);
+
+      screen.debug(nameTextField);
       expect(nameTextField).toBeInTheDocument();
+      expect(nameTextField).toHaveValue(name);
     });
   });
 });
