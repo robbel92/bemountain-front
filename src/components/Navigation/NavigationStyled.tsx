5485b407dab2451fc0d6fb5f0e44a533d6953cd1
Robertt Belenguer Cotaina <rob.belenguer@gmail.com>
2023-06-14 12:03:38 +0200

    Remove animation on navigation

--- src/components/Navigation/NavigationStyled.tsx
+++ src/components/Navigation/NavigationStyled.tsx
@@ -8,19 +8,7 @@ const NavigationStyled = styled.nav`
   padding: 10px 20px;
   align-items: center;
 
-  @keyframes scaleGuai {
-    from {
-      transform: rotate(0);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }
-
   .active {
-    animation-name: scaleGuai;
-    animation-duration: 0.1s;
-    transform: scale(1.2);
     border-radius: 10px;
     border: 3.5px outset #e9c696;
     padding: 1px 5px;
